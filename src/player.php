645869239340385ea502d8788858f47dbe522151
malukenho <admin@phpse.net>
2014-11-14 19:31:25 -0300

    Add more commands as clear and next

--- src/player.php
+++ src/player.php
@@ -5,6 +5,16 @@ use Joomla\Application;
 
 class Player extends Application\AbstractCliApplication
 {
+    /**
+     * @var string
+     */
+    private $genre;
+
+    /**
+     * @var integer contains the music time
+     */
+    private $musicTime;
+
     protected function doExecute()
     {
         $this->out(ASCII\Art::intro());
@@ -19,15 +29,20 @@ class Player extends Application\AbstractCliApplication
             $reflactor = new \ReflectionClass(new Genres\Allowed);
 
             if (false !== array_search($genre[1], $reflactor->getConstants())) {
+                $this->genre = $genre[1];
                 $this->playMusic($genre[1]);
             } else {
                 $this->out('<info>• I\'m sorry Dave, genre not found. </info>');
             }
-        } 
+        } elseif ('next' == $userInput) {
+            $this->playMusic($this->genre);
+        } elseif ('clear' == $userInput) {
+            `clear`;
+        }
 
         $this->doExecute();
     }
-    
+
     protected function playMusic($genre)
     {
         $info = json_decode(file_get_contents('https://cmd.fm/api/tracks/search/?genre=' . $genre . '&limit=1'));
@@ -39,8 +54,13 @@ class Player extends Application\AbstractCliApplication
           open location "$stream?client_id=26fb3c513c8e0e2c18a75e6174f4ca70"
           play
           set visible of every window to false
-        end tell'`;
+          end tell'`;
 
+        $time = `osascript -e 'tell application "iTunes" to time of current track as string'`;
+        $duration = `osascript -e 'tell application "iTunes" to duration of current track as string'`;
+        $position = `osascript -e 'tell application "iTunes" to player position as string'`;
+        $this->out($position);
+        $this->out($time);
         $this->out('<info>Playing track.</info>');
     }
 }
