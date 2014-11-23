1d4d6f038f16178274c0df6e7ca56717fb3c2e14
malukenho <admin@phpse.net>
2014-11-23 13:06:47 -0300

    Fix composer keywords and EOF

---
+++ src/MusicPlayer/Player/Itunes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 14/01/15
+ * Time: 18:23
+ */
+
+class Itunes {
+
+} 
---
+++ src/MusicPlayer/Player/PlayerInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 14/01/15
+ * Time: 18:24
+ */
+
+namespace MusicPlayer\Player;
+
+
+class PlayerInterface {
+
+} 
